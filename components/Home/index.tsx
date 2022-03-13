ca3450e887c89aa82484edf2d1894314c47333be
= <=>
2022-03-13 14:24:06 +0530

    fixed the flow when not logged in

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,8 +1,11 @@
 import { NextPage } from 'next';
 import { useSession, signIn } from 'next-auth/react';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
   return (
     <main className="h-screen flex flex-col items-center bg-gamma">
       <section className="h-1/2 flex justify-content-center items-center p-10">
@@ -11,11 +14,12 @@ const Home: NextPage = () => {
         </p>
       </section>
       <section className="flex justify-content-center items-center">
-        <Link href="/getstarted" passHref>
-          <button className="px-10 py-8 border-2 bg-alpha shadow-md rounded-lg text-3xl font-semibold">
-            Begin
-          </button>
-        </Link>
+        <button
+          className="px-10 py-8 border-2 bg-alpha shadow-md rounded-lg text-3xl font-semibold"
+          onClick={() => (session ? router.push('/getstarted') : signIn())}
+        >
+          Begin
+        </button>
       </section>
     </main>
   );
